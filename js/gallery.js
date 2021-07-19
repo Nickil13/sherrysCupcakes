6331666628744ae2e0d117d87d13182598e55345
Nickil13 <36387179+Nickil13@users.noreply.github.com>
2021-07-19 01:37:54 -0600

    commit

---
+++ js/gallery.js
@@ -0,0 +1,22 @@
+const galleryContainer = document.querySelector(".gallery-container");
+
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                loadSlider(JSON.parse(xhttp.responseText));
+            }
+        };
+        xhttp.open("GET","js/gallery-images.json",true);
+        xhttp.send();
+        
+        const loadSlider = (data) =>{
+            data.forEach((item)=>{
+                let content = `<div class="gallery-image"
+                style="background-image:url(${item.image});
+                background-size: cover; background-repeat:no-repeat;
+                background-position: 50% 30%;">
+                </div>`
+                galleryContainer.innerHTML+=content;
+            });
+        }
